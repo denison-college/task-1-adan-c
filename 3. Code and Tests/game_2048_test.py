60c1b9c8eb62f58a11c75ddc3fb24224e76db335
Adan Coello <adan.coello@education.nsw.gov.au>
2026-03-19 10:43:55 +1100

    edit

--- 3. Code and Tests/game_2048_test.py
+++ 3. Code and Tests/game_2048_test.py
@@ -20,9 +20,7 @@ def test_create_grid():
 
 
 def test_display_intructions(monkeypatch, capsys):
-    # prevent recursion into main_menu
     monkeypatch.setattr(game_2048, "main_menu", lambda: None)
-    # simulate pressing Enter
     monkeypatch.setattr("builtins.input", lambda _: "")
 
     game_2048.display_intructions()
@@ -32,7 +30,6 @@ def test_display_intructions(monkeypatch, capsys):
 
 
 def test_main_menu_easy(monkeypatch, capsys):
-    # Simulate: choice = "1" (Play), diff = "1" (Easy)
     inputs = iter(["1", "1"])
     monkeypatch.setattr("builtins.input", lambda _: next(inputs))
 
@@ -40,12 +37,10 @@ def test_main_menu_easy(monkeypatch, capsys):
     out = capsys.readouterr().out
 
     assert "Easy mode selected" in out
-    # 6x6 grid row
     assert "[0, 0, 0, 0, 0, 0]" in out
 
 
 def test_main_menu_medium(monkeypatch, capsys):
-    # Simulate: choice = "1" (Play), diff = "2" (Medium)
     inputs = iter(["1", "2"])
     monkeypatch.setattr("builtins.input", lambda _: next(inputs))
 
@@ -53,12 +48,10 @@ def test_main_menu_medium(monkeypatch, capsys):
     out = capsys.readouterr().out
 
     assert "Medium mode selected" in out
-    # 5x5 grid row
     assert "[0, 0, 0, 0, 0]" in out
 
 
 def test_main_menu_hard(monkeypatch, capsys):
-    # Simulate: choice = "1" (Play), diff = "3" (Hard)
     inputs = iter(["1", "3"])
     monkeypatch.setattr("builtins.input", lambda _: next(inputs))
 
@@ -66,26 +59,19 @@ def test_main_menu_hard(monkeypatch, capsys):
     out = capsys.readouterr().out
 
     assert "Hard mode selected" in out
-    # 4x4 grid row
     assert "[0, 0, 0, 0]" in out
 
 
 def test_main_menu_instructions(monkeypatch, capsys):
-    # prevent recursion from display_intructions -> main_menu
     monkeypatch.setattr(game_2048, "main_menu", lambda: None)
-
-    # Simulate: choice = "2" (Instructions), then Enter
     inputs = iter(["2", ""])
     monkeypatch.setattr("builtins.input", lambda _: next(inputs))
-
     game_2048.main_menu()
     out = capsys.readouterr().out
-
     assert "Use W/A/S/D to move tiles and combine numbers to reach 2048!" in out
 
 
 def test_main_menu_invalid(monkeypatch, capsys):
-    # Simulate: invalid menu choice
     monkeypatch.setattr("builtins.input", lambda _: "9")
 
     game_2048.main_menu()
