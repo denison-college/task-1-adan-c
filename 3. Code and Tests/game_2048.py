59391f38e5467546b55b015e348a9f6d828765ed
Adan coello <adan.coello@education.nsw.gov.au>
2026-03-21 14:41:22 +1100

    fixed tetst code

--- 3. Code and Tests/game_2048.py
+++ 3. Code and Tests/game_2048.py
@@ -1,7 +1,7 @@
 def display_intructions():
     print("Use W/A/S/D to move tiles and combine numbers to reach 2048!")
     input("\nPress Enter to return to the menu...")
-    main_menu()   
+     
 
 def choose_difficulty():
     print("Choose difficulty:")
@@ -43,7 +43,7 @@ def main_menu():
 
     elif choice == "2":
         display_intructions()
-                
+        main_menu() 
 
     else:
         print("Invalid choice.")
